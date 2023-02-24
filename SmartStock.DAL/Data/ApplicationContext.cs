952d2cc64ffb12f8d73e9e4032a400e8f06c17c7
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-24 20:29:31 +0200

    added classes for db connection

---
+++ SmartStock.DAL/Data/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartStock.DAL.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public static OptionsBuilder OptionsBuilder => new OptionsBuilder();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                ApplicationConfiguration configuration = new ApplicationConfiguration();
+                optionsBuilder.UseSqlServer(configuration.SqlConnectionString);
+            }
+        }
+    }
+}
---
+++ SmartStock.DAL/Data/OptionsBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmartStock.DAL.Data
+{
+    public class OptionsBuilder
+    {
+        public OptionsBuilder()
+        {
+            _configuration = new ApplicationConfiguration();
+            DbContextOptionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            DbContextOptionsBuilder.UseSqlServer(_configuration.SqlConnectionString);
+            DbContextOptions = DbContextOptionsBuilder.Options;
+        }
+
+        public DbContextOptionsBuilder<ApplicationContext> DbContextOptionsBuilder { get; set; }
+
+        public DbContextOptions<ApplicationContext> DbContextOptions { get; set; }
+
+        private ApplicationConfiguration _configuration { get; set; }
+    }
+}
