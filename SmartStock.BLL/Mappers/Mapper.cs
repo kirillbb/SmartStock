36c1129a83ec3cdb30af3c30321ae28f60122655
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-25 15:37:12 +0200

    generic mapper added

---
+++ SmartStock.BLL/Mappers/Mapper.cs
@@ -0,0 +1,23 @@
+﻿namespace SmartStock.BLL.Mappers
+{
+    public static class Mapper<TSource, TDestination>
+    {
+        public static TDestination Map(TSource source)
+        {
+            var destination = Activator.CreateInstance<TDestination>();
+            var sourceProperties = typeof(TSource).GetProperties();
+            var destinationProperties = typeof(TDestination).GetProperties();
+
+            foreach ( var property in sourceProperties )
+            {
+                var destinationProperty = destinationProperties.FirstOrDefault(x => x.Name == property.Name && x.PropertyType == property.PropertyType);
+                if ( destinationProperty != null )
+                {
+                    destinationProperty.SetValue(destination, property.GetValue(source));
+                }
+            }
+
+            return destination;
+        }
+    }
+}
