f44dc515912d0885dcdf7f3274b758084b9ac86f
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-25 13:36:10 +0200

    added generic repo

--- SmartStock.CORE/Entities/User.cs
+++ SmartStock.CORE/Entities/User.cs
@@ -3,12 +3,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SmartStock.CORE.Entities
 {
-    internal class User
+    public class User
     {
         [Key]
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public string Adress { get; set; }
 
         public ICollection<Order>? Orders { get; set; }
     }
