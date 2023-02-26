446431e97e81aabd38862b84d0001a6e0f9ebf58
Kirill_divine <kirill.sokolov96@gmail.com>
2023-02-26 13:55:37 +0200

    rework domain models

--- SmartStock.CORE/Entities/OrderType/Order.cs
+++ SmartStock.CORE/Entities/OrderType/Order.cs
@@ -1,27 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartStock.CORE.Entities.OrderType
 {
     public class Order
     {
         [Key]
-        [Column("OrderId")]
         public int Id { get; set; }
 
         [Required]
-        public string CustomerId { get; set; }
+        public int UserId { get; set; }
+
+        public UserOrder UserOrder { get; set; }
 
         [Required]
         public string CustomerAddress { get; set; }
 
+        public DateTime Date { get; set; }
 
         [Required]
         public decimal TotalPrice { get; set; }
 
         [Required]
         public OrderStatus Status { get; set; }
-        
+
         public ICollection<OrderProduct> OrderProducts { get; set; }
     }
 }
